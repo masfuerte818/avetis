426652676c5c0577071637d2d2cdbb899a602de3
masfuerte818 <67984099+masfuerte818@users.noreply.github.com>
2021-01-12 18:16:36 +0400

    initial

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const path = require('path')
 
 const app = express();
 
@@ -8,7 +9,14 @@ app.use(express.json({ extended: true }));
 app.use('/api/receive', require('./routes/receive.routes'));
 app.use('/api/present', require('./routes/present.routes'));
 
-const PORT = config.get('port') || 3000;
+const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 async function start() {
     try {
